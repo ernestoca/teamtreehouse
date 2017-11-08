316f92c3657bf896b52a3afb3202c31319e4376d
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2017-11-08 18:29:06 -0500

    javascript arrow function

--- jQuery/simple-drawing-app/js/app.js
+++ jQuery/simple-drawing-app/js/app.js
@@ -7,12 +7,12 @@ var lastEvent;
 var mouseDown = false;
 
 //When clicking on control list items
-$('.controls li').click(function() {
+$('.controls').on("click", "li", function() {
     //Deselect sibling elements
     $(this).siblings().removeClass('selected');    
     //Select clicked elements
     $(this).addClass('selected');
-    
+    color = $(this).css("background-color");
 });
 
 //When new color is pressed
@@ -23,24 +23,48 @@ $('#revealColorSelect').click(function() {
 });
 
 function changeColor() {
-    var r = $("#red").val();
-    var g = $("#green").val();
-    var b = $("#blue").val();
-    //Update the new color span
-    $('#newColor').css('background-color', "rbg("+r+","+g+","+b+")");
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
 }
 
 //When color sliders change
 $('input[type=range]').change(changeColor);
 
-//When add color is pressed
-    //Append the color to the controls ul
-    //Select the new color
+changeColor();
 
+//When add color is pressed
+$('#addNewColor').click(function() {
+    //Append the color to the controls ul
+   var $newColor = $('<li></li>');
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
+    //Select the new color
+    $newColor.click();
+    
+});
 
 //On mouse event on the canvas
+$canvas.mousedown(function(e) {
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function(e) {
     //Draw lines
-
+    if(mouseDown) {
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = color;
+        context.stroke();
+        lastEvent = e;   
+    }    
+}).mouseup(function() {
+    mouseDown = false;
+}).mouseleave(function() {
+    $canvas.mouseup();
+});
+    
   
 
 
