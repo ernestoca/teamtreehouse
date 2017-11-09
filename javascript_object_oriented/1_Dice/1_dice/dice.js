5be197704349978a87dec80f3f6e586601c1a170
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2017-11-09 18:21:37 -0500

    calculator

--- javascript_object_oriented/1_Dice/1_dice/dice.js
+++ javascript_object_oriented/1_Dice/1_dice/dice.js
@@ -5,5 +5,9 @@ function diceRoll() {
 }
 
 var dice = {
-
+    sides: 6,
+    roll: function() {
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+    }
 }
