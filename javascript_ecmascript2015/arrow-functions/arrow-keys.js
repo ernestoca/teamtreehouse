e234080ffc402f84a1ba94ae8506e665a5309df2
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2017-11-14 19:42:01 -0500

    arrow functions

--- javascript_ecmascript2015/arrow-functions/arrow-keys.js
+++ javascript_ecmascript2015/arrow-functions/arrow-keys.js
@@ -4,7 +4,10 @@ var Person = function (data) {
   for (var key in data) {
     this[key] = data[key];  
   }
-  this.getKeys = function () {
+  this.getKeys = () => {
+    //So what the arrow, or lambda function does, is it binds the function to 
+    //the instance of the person, no matter where it was called.
+    //In other words, this now refers to Alena.
     return Object.keys(this);
   }
 }
@@ -16,3 +19,5 @@ console.log('Alena\'s Keys:', Alena.getKeys()); // 'this' refers to 'Alena'
 var getKeys = Alena.getKeys;
 
 console.log(getKeys()); // 'this' refers to the node process
+
+//An arrow function is bound to his parent scope
